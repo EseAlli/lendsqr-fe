b2c6d1222ae597b519aa9161ccd829b7e5b92fec
EseAlli <essealli.ea@gmail.com>
2022-08-06 00:22:51 +0100

    all page skeleton created

--- lendsqr-fe/src/pages/User/User.jsx
+++ lendsqr-fe/src/pages/User/User.jsx
@@ -3,14 +3,14 @@ import Card from "../components/Card";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 
-const Dashboard = () => {
+const User = () => {
   return (
     <div>
       <Navbar />
       <Sidebar />
       <div className="main-container">
         <div className="contain">
-          <p className="page-title">Dashboard</p>
+          <p className="page-title">Users</p>
           <div className="card-container">
             <Card />
             <Card />
@@ -23,4 +23,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default User;
