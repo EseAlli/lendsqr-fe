eb9e442f3ec449e7a95a0c2f2be4f2d4b5d64def
EseAlli <essealli.ea@gmail.com>
2022-08-06 13:18:22 +0100

    user details design implementation

--- lendsqr-fe/src/App.js
+++ lendsqr-fe/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import "./styles/app.scss"
 import User from "./pages/User/User";
+import UserDetails from "./pages/User/UserDetails";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path='/' element={ <Login/> }/>
         <Route exact path='/dashboard' element ={<Dashboard/>}/>
         <Route path='/users' element ={<User/>}/>
+        <Route path='/users/id' element={<UserDetails/>}/>
       </Routes>
     </BrowserRouter>
   );
--- lendsqr-fe/src/pages/User/UserDetails.jsx
+++ lendsqr-fe/src/pages/User/UserDetails.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Card from "../components/Card";
-import Sidebar from "../components/Sidebar";
-import Navbar from "../components/Navbar";
+import Card from "../../components/Card";
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+import avatar from "../../assets/icons/avatar.svg";
 
 const UserDetails = () => {
   return (
@@ -10,12 +11,204 @@ const UserDetails = () => {
       <Sidebar />
       <div className="main-container">
         <div className="contain">
-          <p className="page-title">Users</p>
-          <div className="card-container">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+          <p className="page-title">User Details</p>
+          <div className="user-action-btns">
+            <button className="btn btn-outline btn-outline-red">
+              Blacklist User
+            </button>
+            <button className="btn btn-outline btn-outline-green">
+              Activate User
+            </button>
+          </div>
+          <div className="user-details">
+            <div className="shadow-box">
+              <div className="basic-info">
+                <div className="flex gap-6 info">
+                  <img src={avatar} />
+                  <div>
+                    <p className="user-name">Grace Effiom</p>
+                    <p>lsdjgekh</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="info tier">
+                  <p className="title">User's Tier</p>
+                  <p>lsdjgekh</p>
+                </div>
+                <hr />
+                <div className="info account">
+                  <p className="title">₦200,000.00</p>
+                  <p>9912345678/Providus Bank</p>
+                </div>
+              </div>
+              <div className="tab-controls">
+                <ul className="tab-list">
+                  <li className="active">General Details</li>
+                  <li>Documents</li>
+                  <li>Bank Details</li>
+                  <li>Loans</li>
+                  <li>Savings</li>
+                  <li>App and System</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="tabs shadow-box">
+              <h3 className="tab-accordion-header">Documents</h3>
+              <div className="tab-content">
+                <p className="section-title">Personal Information</p>
+                <div className="section">
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                </div>
+                <hr />
+                <p className="section-title">Personal Information</p>
+                <div className="section">
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                </div>
+                <hr />
+                <p className="section-title">Personal Information</p>
+                <div className="section">
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                </div>
+                <hr />
+                <p className="section-title">Personal Information</p>
+                <div className="section">
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                  <div className="section-content">
+                    <p className="label">Full Name</p>
+                    <p className="value">Grace Effiom</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
