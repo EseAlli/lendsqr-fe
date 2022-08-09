797eb9e291d9fcd9eb630ce4bb3c9cd33cfd583d
EseAlli <essealli.ea@gmail.com>
2022-08-09 08:43:35 +0100

    style changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 import "./styles/app.scss"
 import User from "./pages/User/User";
 import UserDetails from "./pages/User/UserDetails";
@@ -13,7 +12,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route exact path='/' element={ <Login/> }/>
-        <Route exact path='/dashboard' element ={<Dashboard/>}/>
+        <Route exact path='/dashboard' element ={<User/>}/>
         <Route path='/users' element ={<User/>}/>
         <Route path='/users/:id' element={<UserDetails/>}/>
       </Routes>
